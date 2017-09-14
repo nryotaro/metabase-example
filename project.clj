8128b3eb5234c7d504006ae6be215b3f241eeaa2
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-09-14 18:48:56 +0900

    fixed

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  [com.damballa/abracad "0.4.13"]
+                 [clj-time "0.14.0"]
                  [cheshire "5.8.0"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}})
--- src/rna/core.clj
+++ src/rna/core.clj
@@ -1,9 +1,28 @@
 (ns rna.core
-  (:require [abracad.avro :as avro]))
-
+  (:require [abracad.avro :as avro]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]
+            [clj-time.format :as f]
+            [clj-time.coerce :as c]
+            [clojure.java.io :as io]))
 
 (defn deserialize
   [file-path]
   (with-open [adf (avro/data-file-reader file-path)]
     (doall (seq adf))))
 
+(s/fdef list-files
+        :args (s/cat :dir-path string?))
+(defn list-files
+  [dir-path]
+  (filter #(. % isFile) (-> dir-path io/file file-seq)))
+
+
+#_(defn store-as-file
+  [map]
+  nil)
+
+
+; (f/unparse (f/formatter "yyyyMMdd") (c/from-long 1450656000000))
+
+(stest/instrument)
